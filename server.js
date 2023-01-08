299570633c62ec7ccb1e9828bb535edfcf93eb30
farhat982 <112558257+farhat982@users.noreply.github.com>
2023-01-08 07:31:11 +0000

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   next();
 });
-app.use(cors({ origin: 'http://localhost:3000' }));
+
+app.use(
+	cors({ origin: 'https://social-media-backend-i91c.onrender.com', credentials: true })
+);
 app.use(express.json());
 app.use(cookieParser());
 
